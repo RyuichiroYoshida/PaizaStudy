1dd95f6d0d2f1cc962bca2ca742d4cb0dd3518de
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-10-23 04:44:18 +0900

    プロジェクト ファイルを追加します。

---
+++ ProgStudy1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        if (n % 15 == 0)
+            Console.WriteLine("FizzBuzz");
+        else if (n % 5 == 0)
+            Console.WriteLine("Buzz");
+        else if (n % 3 == 0)
+            Console.WriteLine("Fizz");
+        else
+            Console.WriteLine(n);
+    }
+}
