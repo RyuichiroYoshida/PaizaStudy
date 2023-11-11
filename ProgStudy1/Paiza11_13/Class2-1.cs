fab4607095a1aff56adf8a3695005687a2e86cb8
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-11-11 16:54:03 +0900

    paizaの森 課題終了

---
+++ ProgStudy1/Paiza11_13/Class2-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Paiaza11_13;
+class Class2_1
+{
+    static void Main()
+    {
+        var line = Console.ReadLine();
+        switch (line)
+        {
+            case "baseball":
+                Console.WriteLine(9);
+                break;
+            case "soccer":
+                Console.WriteLine(11);
+                break;
+            case "rugby":
+                Console.WriteLine(15);
+                break;
+            case "basketball":
+                Console.WriteLine(5);
+                break;
+            case "volleyball":
+                Console.WriteLine(6);
+                break;
+        }
+    }
+}
---
+++ ProgStudy1/Paiza11_13/Class2-2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Paiaza11_13;
+class Class2_2
+{
+    static void Main()
+    {
+        var hour = int.Parse(Console.ReadLine());
+        if (hour <= 6)
+            Console.WriteLine("no break");
+        else if (hour <= 8)
+            Console.WriteLine("45 min");
+        else
+            Console.WriteLine("60 min");
+    }
+}
